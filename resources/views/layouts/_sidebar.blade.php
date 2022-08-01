d0683af8327ea15c8f1ae31cda6776c374cf6a93
omar96i <sagara9930@hotmail.com>
2022-08-01 16:12:29 -0500

    update default image navbar

--- resources/views/layouts/_sidebar.blade.php
+++ resources/views/layouts/_sidebar.blade.php
@@ -2,9 +2,9 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('home')}}">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <img src="{{asset('img/icons/icon.png')}}" width="100">
-        </div>
+
+            <img src="{{asset('img/icons/icono.jpeg')}}" class="img-fluid">
+
     </a>
 
     <!-- Divider -->
