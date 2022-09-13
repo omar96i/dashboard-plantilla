62b40f8c96b224df3d7e1354a8403d029cd03fac
omar96i <sagara9930@hotmail.com>
2022-09-13 09:34:15 -0500

    update

--- resources/views/layouts/_scripts.blade.php
+++ resources/views/layouts/_scripts.blade.php
@@ -4,9 +4,6 @@
 <!-- Core plugin JavaScript-->
 <script src="{{ asset('plugins/jquery-easing/jquery.easing.min.js') }}"></script>
 
-<!-- Custom scripts for all pages-->
-<script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
-
 <!-- Page level plugins -->
 <script src="{{ asset('plugins/chart.js/Chart.min.js') }}"></script>
 
