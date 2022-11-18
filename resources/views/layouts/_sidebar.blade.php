58267bb2647d22ce10ea7c429a11dca138730881
omar96i <sagara9930@hotmail.com>
2022-11-18 08:43:58 -0500

    update

--- resources/views/layouts/_sidebar.blade.php
+++ resources/views/layouts/_sidebar.blade.php
@@ -102,6 +102,20 @@
         </div>
     </li>
 
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapse-roles"
+            aria-expanded="true" aria-controls="collapse-roles">
+            <i class="fa-solid fa-scale-balanced"></i>
+            <span>Roles</span>
+        </a>
+        <div id="collapse-roles" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Administrar</h6>
+                <a class="collapse-item" href="#">Roles</a>
+            </div>
+        </div>
+    </li>
+
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item @if(Route::is('configuraciones.index')) active @endif">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapse-five"
